90019d55df9513769c7ea5ef28c6d31726a7af2d
mahendragalih26 <mahendragalih26@gmail.com>
2024-03-06 19:39:31 +0700

    add : header

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 
 import { AnimatePresence } from "framer-motion"
 import Preloader from "../components/Preloader"
+import Header from "../components/Header"
 import Landing from "../components/Landing"
 import Description from "../components/Description"
 import Projects from "../components/Projects"
@@ -31,6 +32,7 @@ export default function Home() {
       <AnimatePresence mode="wait">
         {isLoading && <Preloader />}
       </AnimatePresence>
+      <Header />
 
       <Landing />
 
